fabcb06105428bcc8a3ab6f8dfe0eaa6c8711749
tangly <tlyong1992@hotmail.com>
2021-05-18 16:26:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'TANG',
+  author: 'tangHH',
+  email: 'tlyong1992@hotmail.com',
+  link: 'https://tangly1024.tk',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
   appearance: 'auto', // ['light', 'dark', 'auto'],
