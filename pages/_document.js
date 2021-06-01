252223066e47dac764e0bfdeaa5f0e228546abda
tlyong1992 <tlyong1992@hotmail.com>
2021-06-01 14:36:51 +0800

    谷歌字体转为国内镜像

--- pages/_document.js
+++ pages/_document.js
@@ -54,28 +54,33 @@ class MyDocument extends Document {
             BLOG.lang.slice(0, 2).toLocaleLowerCase()
           ) && (
             <>
+              {/* 国内谷歌字体镜像 
+                    fonts.gstatic.com = fonts-gstatic.proxy.ustclug.org
+                    fonts.googleapis.com = fonts.proxy.ustclug.org
+                */}
               <link
                 rel="preconnect"
-                href="https://fonts.gstatic.com"
+                href="https://fonts-gstatic.proxy.ustclug.org"
                 crossOrigin="anonymous"
               />
+              
               <link
                 rel="preload"
                 as="style"
-                href={`https://fonts.googleapis.com/css2?family=Noto+${
+                href={`https://fonts.proxy.ustclug.org/css2?family=Noto+${
                   BLOG.font === 'serif' ? 'Serif' : 'Sans'
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <link
                 rel="stylesheet"
-                href={`https://fonts.googleapis.com/css2?family=Noto+${
+                href={`https://fonts.proxy.ustclug.org/css2?family=Noto+${
                   BLOG.font === 'serif' ? 'Serif' : 'Sans'
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <noscript>
                 <link
                   rel="stylesheet"
-                  href={`https://fonts.googleapis.com/css2?family=Noto+${
+                  href={`https://fonts.proxy.ustclug.org/css2?family=Noto+${
                     BLOG.font === 'serif' ? 'Serif' : 'Sans'
                   }+${CJK()}:wght@400;500;700&display=swap`}
                 />
