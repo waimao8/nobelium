ea05d04b496f3f808a01a5af513bd13b3e9fa5f5
tlyong1992 <tlyong1992@hotmail.com>
2021-06-15 09:23:33 +0800

    新增首页导航

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,8 @@ const NavBar = () => {
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 3, name: locale.NAV.NAVGATION, to: 'https://search.tangly1024.com/', show: true },
+    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
