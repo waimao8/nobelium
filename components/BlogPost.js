3c35b83a9fd81a0f41129a961cd4cbcde9cc4e64
tlyong1992 <tlyong1992@hotmail.com>
2021-06-22 21:39:33 +0800

    夜间：首页列表颜色

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -10,7 +10,7 @@ const BlogPost = ({ post }) => {
             <img className="h-52 w-full md:h-44 md:w-56 object-cover" src={post.page_cover ?? post.page_image}
                  alt={post.title}/>
           </Link>
-          <main className="p-8 dark:text-gray-300">
+          <main className="p-8">
             <a href={`${BLOG.path}/${post.slug}`} className="block mt-1 text-xl leading-tight font-medium text-black dark:text-gray-200 hover:underline">{post.title}</a>
             <div className="uppercase tracking-wide text-sm text-indigo-300 font-semibold">{formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}</div>
             <p className="mt-2 text-gray-400">{post.summary}</p>
