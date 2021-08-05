8c553bd0e239110190144f9ba47e18a8844e2a8f
waimao8 <55073834+waimao8@users.noreply.github.com>
2021-08-05 12:08:59 +0800

    Update [slug].js

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -5,7 +5,7 @@ import { createHash } from 'crypto'
 import { getPageTableOfContents } from 'notion-utils'
 
 const BlogPost = ({ post, blockMap, emailHash }) => {
-  if (!post) return null
+  if (!post) return <div>404</div>
   return (
     <Article
       blockMap={blockMap}
